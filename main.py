e752d884eb01894b18338566c712c3bd42e53332
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-30 21:43:29 +0530

    demo3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from_whatsapp_no = os.environ['from_whatsapp_no']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Code Pushed in main branch of https://github.com/G-Sudarshan/Github-to-Whatsapp-Push-Notifier',
+                              body='Code Pushed in main branch of https://github.com/Tech-Squad-2021/github-whatsapp-bot',
                               from_='whatsapp:'+from_whatsapp_no,
                               to='whatsapp:'+to_whatsapp_no
                           )
